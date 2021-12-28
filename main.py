2d5a869698202f7e6054386b69e6ac057a23082f
Jimmy Stoyell <jstoyell@fb.com>
2021-12-27 21:23:56 -0500

    add command line args

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import random
+import argparse
+
 from game import *
 from player import *
 from players.basic import *
 from players.dumbo import *
 
 from constants import *
-
-import random
 
 PLAYERS = [
     DumboPlayer(),
@@ -16,6 +17,11 @@ PLAYERS = [
 
 random.shuffle(PLAYERS)
 
-game = Game(PLAYERS, True, True)
 
-game.play_game()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='No Thanks game engine.')
+    parser.add_argument('-v','--verbose', dest='verbose', action='store_const', const=True, default=False, help='Use verbose mode')
+    parser.add_argument('-m','--manual', dest='manual', action='store_const', const=True, default=False, help='Use manual mode')
+    args = parser.parse_args()
+    game = Game(PLAYERS, args.verbose, args.manual)
+    game.play_game()
